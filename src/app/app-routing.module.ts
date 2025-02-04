b10fe1c3d9208b42e3141536c7d519bcc3946baf
Rushil Shah <rushils@Meditab.Local>
2025-02-04 14:25:02 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: RegisterComponent},
+  {path:'login', component: LoginComponent},
+  {path:'admin-dashboard', component: AdminDashboardComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
